9bba52fe56ea9d2060d32667e97061afd1605574
Sukrit Saha <sukritsaha27@gmail.com>
2023-08-23 15:54:10 +0530

    adding about page

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -1,5 +1,84 @@
+import Link from "next/link";
+
+import { features, socials, techStack } from "@/config";
+import { Separator } from "@/components/ui/separator";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <section className="container max-w-3xl pb-8 md:pb-10 space-y-8">
+      <div>
+        <div className="grid gap-1">
+          <h1 className="line-clamp-1 text-3xl font-bold tracking-tight py-1">
+            About
+          </h1>
+          <p className="text-muted-foreground text-md">
+            About the project and the author of the project.
+          </p>
+        </div>
+        <Separator className="my-4" />
+        <p className="font-light">
+          Revolutionize your self-presentation with our Next.js web app, the
+          result of my frontend intern assignment from &rsquo;Codedamn&rsquo;.
+          Craft compelling job profiles effortlessly using Figma&rsquo;s design
+          as the foundation. Showcase images, projects, certificates,
+          experiences, and education seamlessly for a standout professional
+          portfolio.
+        </p>
+      </div>
+
+      <div>
+        <h1 className="text-xl font-semibold tracking-tight">
+          Tech stack used
+        </h1>
+        <Separator className="my-2" />
+        <ul className="space-y-2 mx-5 mt-2">
+          {techStack.map((tech, index) => (
+            <li key={index} className="list-disc">
+              <Link
+                href={tech.url}
+                target="_blank"
+                className="underline font-medium tracking-tight underline-offset-4"
+              >
+                {tech.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="text-xl font-semibold tracking-tight">Key features</h1>
+        <Separator className="my-2" />
+        <ul className="space-y-2 mx-5 mt-2">
+          {features.map((feature, index) => (
+            <li key={index} className="list-disc">
+              {feature}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="text-xl font-semibold tracking-tight">
+          About the author
+        </h1>
+        <Separator className="my-2" />
+        <ul className="space-y-2 mx-5 mt-2">
+          {socials.map((social) => (
+            <li key={social.id} className="list-disc">
+              <Link
+                href={social.href}
+                target="_blank"
+                className="underline font-medium tracking-tight underline-offset-4"
+              >
+                {social.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default AboutPage;
--- src/config.ts
+++ src/config.ts
@@ -16,3 +16,69 @@ export const sidebarItems = [
     onBoarded: true,
   },
 ];
+
+export const techStack = [
+  {
+    name: "Next.js",
+    url: "https://nextjs.org/",
+  },
+  {
+    name: "Tailwind CSS",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "TypeScript",
+    url: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "Prisma",
+    url: "https://www.prisma.io/",
+  },
+  {
+    name: "MongoDB",
+    url: "https://www.mongodb.com/",
+  },
+  {
+    name: "NextAuth",
+    url: "https://next-auth.js.org/",
+  },
+  {
+    name: "Uploadthing",
+    url: "https://uploadthing.com/",
+  },
+  {
+    name: "Vercel",
+    url: "https://vercel.com/",
+  },
+];
+
+export const features = [
+  "Authentication with NextAuth",
+  "File uploads with Uploadthing",
+  "ORM with Prisma ORM",
+  "Storing data with MongoDB",
+  "Responsive design with Tailwind CSS",
+];
+
+export const socials = [
+  {
+    id: 1,
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/sukrit-saha-b6117a242/",
+  },
+  {
+    id: 2,
+    label: "GitHub",
+    href: "https://github.com/Sukrittt",
+  },
+  {
+    id: 3,
+    label: "Twitter",
+    href: "https://twitter.com/SukritSaha11",
+  },
+  {
+    id: 4,
+    label: "Instagram",
+    href: "https://www.instagram.com/sukrit_04/",
+  },
+];
